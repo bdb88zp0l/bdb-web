c523a790d24edce33e8bce1ec91341e79ac4886f
Md Yasir Arafat <yasirarafat.dev@gmail.com>
2024-11-03 15:13:59 +0600

    case setting

--- app/(components)/(contentlayout)/admin/case-setting/page.tsx
+++ app/(components)/(contentlayout)/admin/case-setting/page.tsx
@@ -1,25 +1,35 @@
 /**
- * The `CaseSetting` component is the main entry point for the case setting page in the admin section of the application.
- * It renders the page header, SEO metadata, and the `CurrencySetting` component, which handles the currency-related settings.
- * This component is used within the `app/(components)/(contentlayout)/admin/case-setting/page.tsx` file.
+ * The CaseSetting component is the main entry point for the case setting page in the admin section of the application.
+ * It renders the page header, SEO metadata, and the CurrencySetting component, which handles the currency-related settings.
+ * This component is used within the app/(components)/(contentlayout)/admin/case-setting/page.tsx file.
  */
 "use client";
 import Pageheader from "@/shared/layout-components/page-header/pageheader";
 import Seo from "@/shared/layout-components/seo/seo";
 import React, { Fragment, useEffect, useState } from "react";
-import Announcement from "@/shared/page-components/dashboard-setting/Announcement";
+import CurrencySetting from "@/shared/page-components/case-setting/CurrencySetting";
+import CaseTeamDesignationSetup from "@/shared/page-components/case-setting/CaseTeamDesignationSetup";
+import CaseStatusSetup from "@/shared/page-components/case-setting/CaseStatusSetup";
+import VatSetup from "@/shared/page-components/case-setting/VatSetup";
+import BillingTypeSetup from "@/shared/page-components/case-setting/BillingTypeSetup";
+import ServiceTypeSetting from "@/shared/page-components/case-setting/ServiceTypeSetting";
 
 const CaseSetting = () => {
   return (
     <Fragment>
-      <Seo title={"Dashboard Setting"} />
+      <Seo title={"Case Setting"} />
       <Pageheader
-        currentpage="Dashboard Setting"
+        currentpage="Case Setting"
         activepage="Admin"
-        mainpage="Dashboard Setting"
+        mainpage="Case Setting"
       />
       <div className="grid grid-cols-12 gap-6">
-        <Announcement />
+        <CurrencySetting />
+        <CaseStatusSetup />
+        <CaseTeamDesignationSetup />
+        <VatSetup />
+        <BillingTypeSetup />
+        <ServiceTypeSetting />
       </div>
     </Fragment>
   );
